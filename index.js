38ba7ed593cc2cf826c5fed4d314a06cbdcbfe1b
Tom Magin <magintc@mac.com>
2021-08-01 21:07:55 +1000

    squash all commits

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+const abc = new Person("Tammy", 56);
+console.log("person", abc);
+
+Person.prototype.occupation = "Physician";
+
+console.log(Person);
+
+console.log("index.js");
+
+console.log("next commit");
